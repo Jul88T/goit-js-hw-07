2e054dee00f1f490a8e19b7d5c84c035df80381a
Julia Tarasenko <tarasenkojul88@gmail.com>
2024-12-11 17:27:22 -0800

    Create task-1.js

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+const categoriesEl = document.querySelector("categories").children.length;
+console.log(`Number of categories: ${categoriesEl}`);
+
+const arrayTitlesCategoriesRef = document.querySelectorAll("h2");
+arrayTitlesCategoriesRef.forEach((title) => {
+  console.log("Category:", title.textContent);
+  console.log("Elements:", title.nextElementSibling.children.length);
+});
