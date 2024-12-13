a74282d894f74ad96d9db37470b379b5f83dae2c
Julia Tarasenko <tarasenkojul88@gmail.com>
2024-12-13 15:56:19 -0800

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(`.login-form`);
 
+form.addEventListener(`submit`, function (event) {
+  event.preventDefault();
+
+  const email = form.elements.email.value.trim();
+  const pasword = form.elements.password.value.trim();
+
+  if (mail === `` || password === ``) {
+    alert(`All form fields must be filled in`);
+    return;
+  }
+
+  const formData = {
+    email: email,
+    password: pasword,
+  };
+
+  console.log(formData);
+
+  form.reset();
+});
