9fe674da00bfac0860d9335aae627d6eb027df19
Julia Tarasenko <tarasenkojul88@gmail.com>
2024-12-14 19:54:29 -0800

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,21 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function hexToRgb(hex) {
+  let r = parseInt(hex.slice(1, 3), 16);
+  let g = parseInt(hex.slice(3, 5), 16);
+  let b = parseInt(hex.slice(5, 7), 16);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+const button = document.querySelector(`.change-color`);
+const colorSpan = document.querySelector(`.color`);
+
+button.addEventListener(`click`, () => {
+  const randomHexColor = getRandomHexColor();
+  const randomRgbColor = hexToRgb(randomHexColor);
+
+  document.body.style.backgroundColor = randomRgbColor;
+  colorSpan.textContent = randomRgbColor;
+});
