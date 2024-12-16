2258a08ddfa1b39db98643b3f1a9119aa9d38a46
Julia Tarasenko <tarasenkojul88@gmail.com>
2024-12-16 15:39:03 -0800

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,7 @@ const nameImput = document.getElementById(`name-input`);
 const nameOutput = document.getElementById(`name-output`);
 
 nameImput.addEventListener(`input`, () => {
-  let inputValue = nameInput.value.trim();
+  let inputValue = nameImput.value.trim();
   if (inputValue === ``) {
     nameOutput.textContent = `Anonymous`;
   } else {
